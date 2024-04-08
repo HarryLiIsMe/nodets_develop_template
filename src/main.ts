c92aa661a93b5baf4ddb774e2c154ca0a0a343e2
HarryLiIsMe <42673017+HarryLiIsMe@users.noreply.github.com>
2024-04-08 22:49:28 +0800

    Update typings.json

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ const logInfo = Debug('info');
 const logErr = Debug('error');
 
 async function main() {
-  try {
-    logInfo('hello');
-    logDebug('world');
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		logInfo('hello');
+		logDebug('world');
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  logErr(error);
-  process.exitCode = 1;
+	logErr(error);
+	process.exitCode = 1;
 });
