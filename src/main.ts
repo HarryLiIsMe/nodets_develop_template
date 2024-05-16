7450c386db6e7899e6e965ce3cd6263017bcb619
HarryLiIsMe <42673017+HarryLiIsMe@users.noreply.github.com>
2024-05-17 05:12:46 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ async function main() {
         logInfo('hello');
         logDebug('world');
     } catch (err1) {
-        logErr('err1:', err1);
+        logErr('err1: ', err1);
     }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((err2) => {
-    logErr('err2:', err2);
+    logErr('err2: ', err2);
     process.exitCode = 1;
 });
